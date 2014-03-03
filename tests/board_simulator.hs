ce49a38516e3f2135dfd4e41ea7c3e151b95803f
segfault <s9gf4ult@gmail.com>
2014-03-03 23:44:24 +0600

    more tests

--- tests/board_simulator.hs
+++ tests/board_simulator.hs
@@ -10,6 +10,7 @@
 
 module Main where
 
+import Control.Monad(forM_)
 import Data.Time
 import Database.Persist.Sql
 import Database.Persist.Sqlite
@@ -29,15 +30,22 @@ startTime = UTCTime
 stopTime :: UTCTime
 stopTime = addUTCTime 3600 startTime -- + one hour
 
+timeRat :: Rational -> UTCTime
+timeRat r | r >= 0 && r <= 1 = addUTCTime t startTime
+          | otherwise = error "timeRat: r must be >= 0 && <= 1"
+  where
+    diff = diffUTCTime stopTime startTime
+    t = fromRational $ r * (toRational diff)
 
-linearTicks :: Money -> Money -> Rational -> [Tick]
-linearTicks startPrice stopPrice tickCount = map makeTick
-                                             $ zip [startPrice,startPrice+step .. stopPrice]
-                                             $ iterate (addUTCTime timeStep) startTime
+
+linearTicks :: (Money, Money) -> (UTCTime, UTCTime) -> Rational -> [Tick]
+linearTicks (startPrice, stopPrice) (begTime, endTime) tickCount = map makeTick
+                                                                   $ zip [startPrice,startPrice+step .. stopPrice]
+                                                                   $ iterate (addUTCTime timeStep) begTime
   where
     makeTick (price, time) = Tick "" "" time price 10
     step = (stopPrice - startPrice) / (fromRational tickCount)
-    timeStep = fromRational $ (toRational $ diffUTCTime stopTime startTime) / tickCount
+    timeStep = fromRational $ (toRational $ diffUTCTime endTime begTime) / tickCount
 
 makeSim :: Money -> Ticker -> IO BoardSimulator
 makeSim startMoney startTickers = do
@@ -54,7 +62,7 @@ insertTicks sim ticks = do
 limitSellOrder :: IO ()
 limitSellOrder = do
   sim <- makeSim 0 1
-  insertTicks sim $ linearTicks 50 150 4000
+  insertTicks sim $ linearTicks (50, 150) (startTime, stopTime) 4000
   _ <- registerLimitOrder sim $ LimitOrder startTime Sell 100 1
   simulateUntil sim stopTime
   money <- moneyAmount sim
@@ -67,20 +75,50 @@ limitSellOrder = do
 limitBuyOrder :: IO ()
 limitBuyOrder = do
   sim <- makeSim 100 0
-  insertTicks sim $ linearTicks 150 50 4000
+  insertTicks sim $ linearTicks (150, 50) (startTime, stopTime) 4000
   _ <- registerLimitOrder sim $ LimitOrder startTime Buy 100 1
   simulateUntil sim stopTime
   money <- moneyAmount sim
   tickers <- tickersAmount sim
   orders <- listOrders sim
   assertBool "money must be close to 0" $ money >= 0 && money < 0.1
-  assertEqual "tickers mube be 1" tickers 1
+  assertEqual "tickers must be 1" tickers 1
   assertEqual "orders must be empty" orders []
+
+getTheProfit :: (BoardSimulator -> IO ()) -> IO ()
+getTheProfit simulateRunner = do
+  sim <- makeSim 100 0
+  insertTicks sim $ concat
+    [linearTicks (100, 40) (startTime, timeRat 0.3) 1000
+    ,linearTicks (40, 160) (timeRat 0.3, timeRat 0.8) 2000
+    ,linearTicks (160, 100) (timeRat 0.8, stopTime) 1000]
+  _ <- registerLimitOrder sim $ LimitOrder startTime Buy 50 1
+  _ <- registerLimitOrder sim $ LimitOrder startTime Sell 150 1
+  simulateRunner sim
+  money <- moneyAmount sim
+  tickers <- tickersAmount sim
+  orders <- listOrders sim
+  assertBool "money > 200" $ money >= 200 && money <= 200.1
+  assertEqual "tickers must be 0" tickers 0
+  assertEqual "orders must be empty" orders []
+
+simulateInParts :: Int -> BoardSimulator -> IO ()
+simulateInParts numparts sim = do
+  forM_ parts $ \endTime -> do
+    simulateUntil sim endTime
+  where
+    pts = max 1 numparts
+    step = 1 / (toRational pts)
+    parts = take pts $ map timeRat $ iterate (+step) step
+
 
 mainGroup :: TestTree
 mainGroup = testGroup "board simulator"
-            [ testCase "sell when grows" $ limitSellOrder
-            , testCase "buy when lows" $ limitBuyOrder]
+            [ testCase "sell when grows" limitSellOrder
+            , testCase "buy when lows" limitBuyOrder
+            , testCase "get the profit in 1 parts" $ getTheProfit $ simulateInParts 1
+            , testCase "get the profit in 6 parts" $ getTheProfit $ simulateInParts 6
+            , testCase "get the profit in 100 parts" $ getTheProfit $ simulateInParts 100]
 
 main :: IO ()
 main = defaultMain mainGroup
